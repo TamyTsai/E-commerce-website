d1bf5760e329f575778a7cf6915de23f41b64ed1
Tamy <tamy8677@gmail.com>
2024-06-08 15:27:29 +0800

    install fontawesome

--- app/frontend/styles/frontend/index.js
+++ app/frontend/styles/frontend/index.js
@@ -1,2 +1,6 @@
 import './application.scss';
 // 雖然在js檔引入scss很奇怪 但webpacker就是這樣運作的
+import '@fortawesome/fontawesome-free/css/all.css'
+// 寫簡寫的話（import '@fortawesome/fontawesome-free';），他會去看你這個套件（@fortawesome/fontawesome-free）的package.json檔案 的 main標籤（key）（因為本檔案為js檔） 所對應的 值（value）
+// package.json： "main": "js/fontawesome.js",
+// 但js/fontawesome.js並沒有引進所有字型、css等 所以還是要手動自己加 寫完整的
