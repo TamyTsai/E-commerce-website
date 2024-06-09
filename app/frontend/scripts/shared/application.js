46dabe923afdff5b1adeac278e0a202a200f391d
Tamy <tamy8677@gmail.com>
2024-06-09 09:11:46 +0800

    hamburger menu js, top menu, footer

---
+++ app/frontend/scripts/shared/application.js
@@ -0,0 +1,25 @@
+// 前後端共用之js
+
+// Bulma漢堡選單js
+// https://bulma.io/documentation/components/navbar/
+document.addEventListener('DOMContentLoaded', () => { // Rails 6 中Turbo links預設為開啟 所以如果用 DOMContentLoaded 可能沒辦法間聽到正確之事件，要改用turbolinks:load
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
