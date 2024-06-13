8700e98f075b42c5ba521726f6dabc9b401ca488
Tamy <tamy8677@gmail.com>
2024-06-13 13:48:10 +0800

    frontside: products controller index action, index view, product_list view, categories controller, show view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
 
     def show # 單一商品分類頁面 對應的action
+        @category = Category.find(params[:id])
+        # 先 根據抓到的id（網址沒用friendly id轉換過 所以其實就是網址上的id） 得知 目前是在哪個 單一商品類別 的 頁面
     end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
     def index # 商品清單頁面 所對應的 action
+        @products = Product.order(updated_at: :desc).includes(:vendor)
+        # 為了顯示 N 筆 商品 所關連的 廠商 的名稱資料，額外多做了 5 次的資料庫查詢（最後一次因為已經查詢過所以是 Cache），
+        # 這就是所謂的 N + 1 查詢（1 次 商品 查詢 + N 次的關連資料查詢），這對資料庫來說是很浪費的。
+        # 在 Rails 裡可使用 includes 方法來減少不必要的資料庫查詢。
     end
 
     def show # 檢視單一商品頁面 所對應的 action
