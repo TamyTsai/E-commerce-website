37429b0e4f42caa2b70eddf33d26483b0f211df5
Tamy <tamy8677@gmail.com>
2024-06-13 16:59:54 +0800

    TDD rspec, add same item to cart, cart item doesn’t increase, but quantity increase(unfinished)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,12 +4,31 @@
 
 class Cart # 所有的資料都不會存在資料庫中，只會存在這個 物件 裡面
 
+    attr_reader :items
+    # 讓@items實體變數 可在 實體外被讀取 其回傳之值
+
+    def initialize # 用此類別（Cart）建立的物件，一建立(.new) 就會直接執行intialize這個特殊方法
+        @items = [] # 新建立一個空陣列
+        # 如果把 @items = [] 寫在add_item中，就會變成每次加商品到購物車，購物車都會先被清空成空陣列
+        # 但總是要有一個陣列可以放商品，所以就寫在initialize中，讓Cart.new時，就有空陣列
+        # 實體變數不能直接在外部被取用（實體變數 在 實體內 可自由取用，在 實體外 就不行了），所以要建立getter或用attr_reader
+    end
+
     def add_item(product_id)
+        @items << product_id # 在陣列的最後面加上 product_id（傳進來的參數）元素（作用如同.push(product_id)）
     end
 
     def empty?
-        false # 省略return之寫法
+        @items.empty?
+        # 陣列本來就有empty?方法可用
+        # empty?會回傳陣列是否為空
+
+        # false # 省略return之寫法
         # 呼叫方法時 預設回傳最後一行
     end
+
+    # def items # getter
+    #     return @items
+    # end
 
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,38 +8,71 @@ require 'rails_helper'
 RSpec.describe Cart, type: :model do
 
   describe "基本功能" do # 寫 describe 或 context（情境）都可以
+
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了" do # it "要測試的內容描述" （此內容描述的字串 會被轉換成 方法）
       # 寫測試之3A原則：Arrange（安排）、Act（操作使用）、Assert（斷言 看看符不符合預期）
       # Arrange（安排）
-      cart = Cart.new # 寫測試的當下還沒有Cart model，但可以假設已經有了
+      cart = Cart.new # 寫測試的當下還沒有Cart model，但可以假設已經有了（先寫下要做什麼事和預期的結果，再一步步實作，邊做邊跑測試，看每一步實作有無符合預期）
       # Act（操作使用）
       cart.add_item(2) # 丟2號商品（product_id = 2）進購物車
       # Assert（斷言 看看符不符合預期）
       expect(cart.empty?).to be false # 因為剛剛有把東西丟進購物車，所以預期現在購物車「不該」是空的
 
-      # $ rspec spec/models/cart_spec.rb 執行結果：
-      # 還沒寫Cart model前
-        # $ rspec spec/models/cart_spec.rb
-        # NameError:
-        # uninitialized constant Cart
-      # 建立Cart model後
-        # NoMethodError:
-        #   undefined method `add_item' for #<Cart:0x000000013f047a00>
-      # 到Cart model建立add_item方法後
-        # NoMethodError:
-        #   undefined method `empty?' for #<Cart:0x000000011039ed68>
-      # 到Cart model建立empty?方法後
-        # expected false
-        #      got nil
-      # 在Cart model empty?方法內容 寫上 false 後
-        # Cart
-        #   基本功能
-        #     可以把商品丟到到購物車裡，然後購物車裡就有東西了
-        
-        # Finished in 0.00712 seconds (files took 0.81915 seconds to load)
-        # 1 example, 0 failures
+      # $ rspec
+      # 會去 跑 spec目錄下 所有 檔名最後為_spec的檔案
+      # $ rspec spec/models/cart_spec.rb （只跑這個檔案）執行結果：
+        # 還沒寫Cart model前
+          # $ rspec spec/models/cart_spec.rb
+          # NameError:
+          # uninitialized constant Cart
+        # 建立Cart model後
+          # NoMethodError:
+          #   undefined method `add_item' for #<Cart:0x000000013f047a00>
+        # 到Cart model建立add_item方法後
+          # NoMethodError:
+          #   undefined method `empty?' for #<Cart:0x000000011039ed68>
+        # 到Cart model建立empty?方法後
+          # expected false
+          #      got nil
+        # 在Cart model empty?方法內容 寫上 false 後
+          # Cart
+          #   基本功能
+          #     可以把商品丟到到購物車裡，然後購物車裡就有東西了
+          
+          # Finished in 0.00712 seconds (files took 0.81915 seconds to load)
+          # 1 example, 0 failures
 
     end
+
+    it "加相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
+      # Arrange（安排）
+      cart = Cart.new
+      # cart.rb有寫initialize方法，讓Cart.new一開始就建立@items = []
+
+      # Act（操作使用）
+      # 加3次1號商品到 購物車，加2次2號商品到 購物車
+      3.times { cart.add_item(1) }
+      2.times {cart.add_item(2)}
+      # {} 為程式碼區塊（block），結合律比do...end強
+
+      # Assert（斷言 看看符不符合預期）
+      expect(cart.items.count).to be 2 # 預期只有2個商品「項目」 # items是陣列（@items = []） 所以可以使用count方法 計算 元素數量
+      expect(cart.items.quantity).to be 3 # 預期 第一個商品項目（1號商品）的數量為3
+
+      # 測試結果
+        # 沒 將@item設定為實體外部可讀取 前
+          # NoMethodError:
+          #   undefined method `items' for #<Cart:0x000000014376e870 @item=[1, 1, 1, 2, 2]>
+        # 將@item設定為實體外部可讀取 後
+          # expected #<Integer:5> => 2 （因為目前是寫@items << product_id 沒有依據商品id做分類，所以放5個商品，就有5個商品項目）
+          #       got #<Integer:11> => 5
+        
+          # Compared using equal?, which compares object identity,
+          # but expected and actual are not the same object. Use
+          # `expect(actual).to eq(expected)` if you don't care about
+          # object identity in this example.
+    end
+
   end
 
   describe "進階功能" do
