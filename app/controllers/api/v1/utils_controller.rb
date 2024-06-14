86de93592fb7148791fd40f0c47d5256ca3df3d1
Tamy <tamy8677@gmail.com>
2024-06-14 15:26:19 +0800

    cart AJAX controller.rb, _navbar view

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -29,17 +29,19 @@ class Api::V1::UtilsController < ApplicationController
 
         # 有找到使用者想丟進購物車的商品 才真的把該商品丟入購物車
         if product
-            cart = Cart.from_hash(session[:cart_tamy])
+            # cart = Cart.from_hash(session[:cart_tamy])
             # .from_hash(session[:cart_tamy]) 把 hash 還原為 購物車物件
             # 如果沒有這個hash的話 預設會建全新購物車
-            cart.add_item(product.code)
+            # 寫一個current_cart方法（會回傳Cart.from_hash(session[:cart_tamy])）在 ApplicationController
+            current_cart.add_item(product.code)
 
-            session[:cart_tamy] = cart.serialize
+            session[:cart_tamy] = current_cart.serialize
             # 將購物車hash存到session中
             # session 可讓使用者跳轉到其他頁面時 仍繼續有 存到session的資料（一直存在瀏覽器中）
             # serialize可將 購物車物件 序列化為hash （自己寫的方法）
+            # current_cart為 全新或有東西的 購物車物件
 
-            render json: { status: 'ok', items: cart.items.count } # 要回給前端的東西（json檔）
+            render json: { status: 'ok', items: current_cart.items.count } # 要回給前端的東西（json檔）
         else
         end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
     # 當遇到 噴ActiveRecord::RecordNotFound錯誤訊息 時，就使用 record_not_found方法
 
     before_action :find_categories, unless: :backend? # 只有在不是後台時 才使用find_categories方法
+
+    helper_method :current_cart # 把current_cart方法匯出，讓views也可以用（變成一個view helper）
     
     private
 
@@ -12,7 +14,6 @@ class ApplicationController < ActionController::Base
         render file: "#{Rails.root}/public/404.html", # 用此檔案渲染頁面 Rails.root為專案根目錄
                layout: false, # layout設定為沒有，不套用公版
                status: 404
-
     end
 
     def find_categories # top menu在 所有 前台頁面 都會在，所以將top menu商品分類排序的方法寫在 application層級 controller
@@ -26,6 +27,14 @@ class ApplicationController < ActionController::Base
         # controller_path 會抓到目前controller所在的路徑
         # 以 / 為分割符號 將路徑字串分為多個元素
         # .first抓到第一個元素
+    end
+
+    def current_cart # 全新或有東西的 購物車物件
+        @cart ||= Cart.from_hash(session[:cart_tamy])
+        # 第一次呼叫此方法時，因為 實體變數@cart之值 尚為nil，所以會把Cart.from_hash(session[:cart_tamy])指定給實體變數@cart
+        # 第二次呼叫此方法時，實體變數@cart已經有值，就不再指定值給其（就不會重複進行 將hash轉回購物車物件 的動作）
+        # .from_hash(session[:cart_tamy]) 把 hash 還原為 購物車物件
+        # 如果沒有這個hash的話 預設會建全新購物車
     end
 
 end
