9bd13b09f527f877cd7fa3396cbd0737269a2f58
Tamy <tamy8677@gmail.com>
2024-06-15 11:30:52 +0800

    clear cart function: cart show view, destroy action, continue to shopping function: cart show view, if cart empty flow control

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,4 +8,12 @@ class CartsController < ApplicationController
     def show
     end
 
+    def destroy # 清空購物車按鈕連結 對應的action
+        session[:cart_tamy] = nil
+        # 將session（存有 由購物車物件 轉換成的 hash）設定成 nil，就清掉購物車了
+        # 抓到session中cart key的value
+        # session[:cart] = nil   session是一個hash，裡面有很多東西，只是用了其中一小部分存放購物車，所以如果清掉整個session，可能會連登入的狀態都清掉
+        redirect_to root_path, notice: '購物車已清空'
+    end
+
 end
